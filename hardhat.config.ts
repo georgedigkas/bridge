e66dbd0020756a9b99883c86efd0af0349d0e88c
George Digkas <george.digkas@mystenlabs.com>
2023-11-30 13:46:49 -0500

    init

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,43 @@
-import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+import { HardhatUserConfig } from "hardhat/config";
+import { alchemyApiKey, mnemonic } from "./secrets.json";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 40000,
+  },
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/<key>",
+      accounts: { mnemonic: mnemonic },
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${alchemyApiKey}`,
+      accounts: { mnemonic: mnemonic },
+    },
+    sepoliasui: {
+      url: `https://eth-rpc.testnet.sui.io:443`,
+      accounts: { mnemonic: mnemonic },
+    },
+  },
 };
 
 export default config;
